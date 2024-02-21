d2faff70124e75fc0abcbeed1230a1b977769ad3
Manarbek <avmanarbek@gmail.com>
2024-02-22 00:43:01 +0700

    Perimeter of square

---
+++ Perimeter_of_square/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double perimeter;
+  cout << "Enter the length of the side of the square: ";
+  cin >> perimeter;
+  cout << "The perimeter of the square: " << 4 * perimeter << '\n';
+
+  return 0;
+}
